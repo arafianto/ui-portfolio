aba95abd4bdb15bdb717c7bc093818872c53bc1a
Ahmad Rafianto <ahmdrafianto@gmail.com>
2025-10-27 17:21:21 +0700

    CMS contentful

---
+++ src/lib/contentful.ts
@@ -0,0 +1,18 @@
+import * as contentful from "contentful";
+import type { EntryFieldTypes } from "contentful";
+
+export interface Project {
+  contentTypeId: "projects",
+  fields: {
+    projectTitle: EntryFieldTypes.Text
+    description: EntryFieldTypes.Text
+}
+}
+
+export const contentfulClient = contentful.createClient({
+  space: import.meta.env.CONTENTFUL_SPACE_ID,
+  accessToken: import.meta.env.DEV
+    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
+    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+  host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
+});
